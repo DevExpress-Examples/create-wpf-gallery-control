5784f940686e97a78a8d2ed038f730b09c48a6ab
DevExpressExampleBot <fakeEmail>
2023-12-06 19:22:16 +0400

    Source auto update [skip ci]

--- VB/GalleryControl_Ex/MainWindow.xaml.vb
+++ VB/GalleryControl_Ex/MainWindow.xaml.vb
@@ -1,6 +1,20 @@
-﻿Class MainWindow 
+Imports System.Windows
+Imports System.Windows.Controls
 
-    Private Sub Gallery_ItemClick(ByVal sender As System.Object, ByVal e As DevExpress.Xpf.Bars.GalleryItemEventArgs)
-        MessageBox.Show("The " + e.Item.Caption + " item has been clicked")
-    End Sub
-End Class
+Namespace GalleryControl_Ex
+
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
+
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+
+        Private Sub Gallery_ItemClick(ByVal sender As Object, ByVal e As DevExpress.Xpf.Bars.GalleryItemEventArgs)
+            MessageBox.Show("The " & e.Item.Caption & " item has been clicked")
+        End Sub
+    End Class
+End Namespace
